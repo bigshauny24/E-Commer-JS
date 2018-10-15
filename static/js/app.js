a051c5c63fe974b2fae7ab2dd594763b26966a56
Shaun Swass <shaunswass@gmail.com>
2018-10-15 11:45:44 -0400

    single product card functioning, TODO: Load all products

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,8 @@ $.get('../../components/header.html', function(res){
   //console.log(res);
   $('#nav').html(res);
 })
+
+//add to cart takes in an ID to identify which product is being added
+function addToCart(id){
+  console.log(id);
+}
